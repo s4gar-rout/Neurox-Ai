933c7e9160eb8d46f763ae0b905e552cad14880b
S4gar Rout <sagarrout599@gmail.com>
2026-04-07 19:47:02 +0530

    feat: implement email service using Resend API for notification delivery

--- Backend/src/service/mail.service.js
+++ Backend/src/service/mail.service.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail({ to, subject, html }) {
     try {
         const data = await resend.emails.send({
-            from: "onboarding@resend.dev",
+           from: "Neurox <onboarding@resend.dev>",
             to,
             subject,
             html,
