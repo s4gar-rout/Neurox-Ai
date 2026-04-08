dc4634d1a5184dec1e38a1197456fce293250aa2
S4gar Rout <sagarrout599@gmail.com>
2026-04-08 10:27:46 +0530

    feat: implement nodemailer service with OAuth2 authentication for email delivery

--- Backend/src/service/mail.service.js
+++ Backend/src/service/mail.service.js
@@ -1,7 +1,11 @@
 import nodemailer from "nodemailer";
 
+
+
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,          // ✅ FIXED
+    secure: false,      // ✅ FIXED
     auth: {
         type: "OAuth2",
         user: process.env.GOOGLE_USER,
